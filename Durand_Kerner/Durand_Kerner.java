940799d08f205c030b0d45b7756f6041e34fe47f
jakob rzeppa <rzeppa.jakob@gmail.com>
2023-10-01 10:59:57 +0200

    Durand_Kerner

--- Durand_Kerner/Durand_Kerner.java
+++ Durand_Kerner/Durand_Kerner.java
@@ -1,42 +1,58 @@
 package Durand_Kerner;
 
 public class Durand_Kerner {
-    // Degree: 2 <=> r1 = r0 - f(r0) / ((r0 - s0)(r0 - t0))
+    private static final double ACCURACY = 0.001;
 
-    private double[] fC; // coefficients of the function f
-    private int degree;
-
-    private Complex[] roots;
-
-    public Durand_Kerner(double... coefficients) {
-        fC = coefficients;
-        this.degree = fC.length;
-        findRoots();
-    }
-
-    public void findRoots() {
-        roots = new Complex[degree]; // R,S,T,...
-        findStartPoints();
-        durandKerner();
-    }
-
-    private void findStartPoints() {
-
-
-        
-    }
-
-    private void durandKerner() {
-
-        // do things
-
-
-        // check for change lower then ...
+    // nC -> numerator coefficients; dP -> denominator points
+    private static Complex calc(Complex lastVal, Polynomial f, Complex[] dP) {
+        // get the value of the numerator
+        Complex nV = f.solve(lastVal);
         
 
+        // get the value of the denominator
+        Complex[] dFs = new Complex[dP.length];
+        for (int i = 0; i < dFs.length; i++) {
+            dFs[i] = Complex.minus(lastVal, dP[i]);
+        }
+        Complex dV = Complex.multiply(dFs);
 
-        // else go on
 
-        durandKerner();
+        // create the fraction
+        Complex fr = Complex.devide(nV, dV);
+
+        // return the new value
+        return Complex.minus(lastVal, fr);
     }
+
+    private static Complex[] startingVals(Polynomial f) {
+
+        return new Complex[f.degree];
+    }
+
+    private static Complex[] recursion(Complex[] roots, Polynomial f) {
+        // perform the calculation for every point
+
+        // check if the |change| is smaller then the ACCURACY
+
+        // else recursion
+        return recursion(roots, f);
+    }
+
+    public static Complex[] durandKerner(Polynomial f) {
+        // perform the recursion with the starting values
+        return recursion(startingVals(f), f);
+    }
+
+
+
+    public static void main(String[] args) {
+        Complex c = new Complex(3, 2);
+        Complex d = c;
+        System.out.println(c.toString());
+        System.out.println(d.toString());
+        d.minus(new Complex(1, 1));
+        System.out.println(c.toString());
+        System.out.println(d.toString());
+    }
+
 }
