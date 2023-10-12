76b19e5214d00825f5daca106c3d2430544442fc
jakob rzeppa <rzeppa.jakob@gmail.com>
2023-10-12 18:48:34 +0200

    dings

--- src/Poly.java
+++ src/Poly.java
@@ -17,6 +17,49 @@ class Poly {
         }
     }
 
+    @Override
+    public String toString() {
+        //edge cases
+		if (coefficients.length == 0) {
+			return "";
+		} if (coefficients.length == 1) {
+			return coefficients[0] + "";
+		} if (coefficients.length == 2) {
+			if (coefficients[0] >= 0) {
+				return coefficients[1] + "x +" + coefficients[0];
+			} else {
+				return coefficients[1] + "x " + coefficients[0];
+			}
+		}
+
+        String out = "";
+		for (int i = coefficients.length - 1; i > 1; i--) {
+			if (coefficients[i] == 0) {
+				continue;
+			}
+			if (coefficients[i] >= 0) {
+				out += "+" + coefficients[i] + "x^(" + i + ") ";
+			} else {
+				out += coefficients[i] + "x^(" + i + ") ";
+			}
+		}
+		if (coefficients[1] >= 0) {
+			if (coefficients[0] >= 0) {
+				out += "+" + coefficients[1] + "x" + " +" + coefficients[0];
+			} else {
+				out += "+" + coefficients[1] + "x" + " " + coefficients[0];
+			}
+		} else {
+			if (coefficients[0] >= 0) {
+				out += coefficients[1] + "x" + " +" + coefficients[0];
+			} else {
+				out += coefficients[1] + "x" + " " + coefficients[0];
+			}
+		}
+
+		return out;
+    }
+
     //TODO documentation
     public Complex solve(Complex val) {
         Complex result = new Complex(0, 0);
